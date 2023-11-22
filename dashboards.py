fec0d344bbbec4bbd37e5027f46a99a88f10b1d1
Michelle Duso <chelleduso@gmail.com>
2023-11-22 12:47:42 -0300

    Gerando minhas informações dados

---
+++ dashboards.py
@@ -0,0 +1,35 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+# Ele inicia o app
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+# Cria a tabela, base de dados.
+df = pd.read_excel("Vendas.xlsx")
+
+# Criando o gráfico
+fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+
+# Construindo o site
+app.layout = html.Div(children=[
+    html.H1(children='Faturamento das Lojas'),
+    html.H2(children='Gráfico com o Faturamento de Todos os Produtos separados por Loja'),
+    html.Div(children='''
+        Obs: Esse gráfico mostra a quantidade de produtos vendidos, não o faturamento.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
